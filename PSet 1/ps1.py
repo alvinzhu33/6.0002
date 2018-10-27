b7bb2d03d19cb1b736d2c60714812beddb79c195
AZ <alvinzhu33@yahoo.com>
2018-10-26 20:40:31 -0400

    go!

--- PSet 1/ps1.py
+++ PSet 1/ps1.py
@@ -2,17 +2,17 @@
 ## 6.0002 Fall 2018
 ## Problem Set 1
 ## Written By: habadio, mkebede
-## Name: 
-## Collaborators: 
-## Time: 
+## Name: Alvin Zhu
+## Collaborators:
+## Time:
 
 
 # Problem 1
 class State():
     """
-    A class representing the election results for a given state. 
-    Assumes there are no ties between dem and gop votes. The party with a 
-    majority of votes receives all the Electoral College (EC) votes for 
+    A class representing the election results for a given state.
+    Assumes there are no ties between dem and gop votes. The party with a
+    majority of votes receives all the Electoral College (EC) votes for
     the given state.
     """
     def __init__(self, name, dem, gop, ec):
@@ -21,7 +21,7 @@ class State():
         name - the 2 letter abbreviation of a state
         dem - number of Democrat votes cast
         gop - number of Republican votes cast
-        ec - number of EC votes a state has 
+        ec - number of EC votes a state has
 
         Attributes:
         self.name - str, the 2 letter abbreviation of a state
@@ -29,40 +29,38 @@ class State():
         self.margin - int, difference in votes cast between the two parties, a positive number
         self.ec - int, number of EC votes a state has
         """
-        pass
-        #TODO
+        self.name = name;
+        self.winner = "dem" if dem > gop else "gop";
+        self.margin = abs(dem - gop);
+        self.ec = ec;
 
     def get_name(self):
         """
         Returns:
-        str, the 2 letter abbreviation of the state  
+        str, the 2 letter abbreviation of the state
         """
-        pass
-        #TODO
+        return self.name;
 
     def get_ecvotes(self):
         """
         Returns:
-        int, the number of EC votes the state has 
+        int, the number of EC votes the state has
         """
-        pass
-        #TODO
+        return self.ec;
 
     def get_margin(self):
         """
-        Returns: 
+        Returns:
         int, difference in votes cast between the two parties, a positive number
         """
-        pass
-        #TODO
+        return self.margin;
 
     def get_winner(self):
         """
         Returns:
         str, the winner of the state, "dem" or "gop"
         """
-        pass
-        #TODO
+        return self.winner;
 
     def __str__(self):
         """
@@ -70,9 +68,8 @@ class State():
         str, representation of this state in the following format,
         "In <state>, <ec> EC votes were won by <winner> by a <margin> vote margin."
         """
-        pass
-        #TODO
-        
+        return "In " + self.name + ", " + str(self.ec) + " EC votes were won by " + self.winner + " by a " + str(self.margin) + " vote margin."
+
     def __eq__(self, other):
         """
         Determines if two State instances are the same.
@@ -80,20 +77,21 @@ class State():
         Note: Allows you to check if State_1 == State_2
 
         Param:
-        other - State object to compare against  
+        other - State object to compare against
 
         Returns:
         bool, True if the two states are the same, False otherwise
         """
-        pass 
-        #TODO
+        if self.name == other.name and self.winner == other.winner and self.margin == other.margin and self.ec == other.ec:
+            return True;
+        return False;
 
 
 # Problem 2
 def load_election_results(filename):
     """
     Reads the contents of a file, with data given in the following tab-delimited format,
-    State   Democrat_votes    Republican_votes    EC_votes 
+    State   Democrat_votes    Republican_votes    EC_votes
 
     Parameters:
     filename - the name of the data file as a string
@@ -101,8 +99,15 @@ def load_election_results(filename):
     Returns:
     a list of State instances
     """
-    pass 
-    #TODO
+    file = open(filename, 'r');
+    file.readline();
+    states = [];
+
+    for line in file:
+        info = line.split();
+        states += [State(info[0], int(info[1]), int(info[2]), int(info[3]))]
+    #print(states)
+    return states;
 
 # Problem 3
 def find_winner(election):
@@ -112,26 +117,38 @@ def find_winner(election):
     to the party with the majority vote.
 
     Parameters:
-    election - a list of State instances 
+    election - a list of State instances
 
     Returns:
     a tuple, (winner, loser) of the election i.e. ('dem', 'gop') if Democrats won, else ('gop', 'dem')
     """
-    pass
-    #TODO
+    votes = {'gop': 0, 'dem':0}
+    for state in election:
+        votes[state.get_winner()] += 1;
+    if votes['gop'] > votes['dem']:
+        return ('gop', 'dem');
+    return ('dem', 'gop')
 
 def states_lost(election):
     """
     Finds the list of States that were lost by the losing candidate (states won by the winning candidate).
-    
+
     Parameters:
-    election - a list of State instances 
+    election - a list of State instances
 
     Returns:
     A list of State instances lost by the loser (won by the winner)
     """
-    pass
-    #TODO
+    lost = [];
+    if find_winner(election) == ('gop', 'dem'):
+        winner = 'gop';
+    else: winner = 'dem';
+
+    for state in election:
+        if state.get_winner() == winner:
+            lost += [state];
+
+    return lost;
 
 def ec_votes_reqd(election, total=538):
     """
@@ -139,28 +156,38 @@ def ec_votes_reqd(election, total=538):
     Note: A party wins when they earn half the total number of EC votes plus 1.
 
     Parameters:
-    election - a list of State instances 
+    election - a list of State instances
     total - total possible number of EC votes
 
     Returns:
     int, number of additional EC votes required by the loser to change the election outcome
     """
-    pass
-    #TODO
-                                         
+    lost = [];
+    votes = 0;
+
+    if find_winner(election) == ('gop', 'dem'):
+        loser = 'dem';
+    else: loser = 'gop';
+
+    for state in election:
+        if state.get_winner() == loser:
+            votes += state.get_ecvotes();
+
+    return total//2 + 1 - votes;
+
 # Problem 4
 def greedy_election(lost_states, ec_votes_needed):
     """
     Finds a subset of lost_states that would change an election outcome if
-    voters moved into those states. First chooses the states with the smallest 
-    win margin, i.e. state that was won by the smallest difference in number of voters. 
-    Continues to choose other states up until it meets or exceeds the ec_votes_needed. 
+    voters moved into those states. First chooses the states with the smallest
+    win margin, i.e. state that was won by the smallest difference in number of voters.
+    Continues to choose other states up until it meets or exceeds the ec_votes_needed.
     Should only return states that were originally lost by the loser (won by the winner) in the election.
 
     Parameters:
     lost_states - a list of State instances that were lost by the loser of the election
     ec_votes_needed - int, number of EC votes needed to change the election outcome
-    
+
     Returns:
     A list of State instances such that the election outcome would change if additional
     voters relocated to those states (also can be referred to as our swing states)
@@ -180,23 +207,23 @@ def dp_move_max_voters(lost_states, ec_votes, memo = None):
     is as large as possible.
 
     Parameters:
-    lost_states - a list of State instances that were lost by the loser 
-    ec_votes - int, the maximum number of EC votes 
+    lost_states - a list of State instances that were lost by the loser
+    ec_votes - int, the maximum number of EC votes
     memo - dictionary, an OPTIONAL parameter for memoization (don't delete!).
     Note: If you decide to use the memo make sure to override the default value when it's first called.
 
     Returns:
     A list of State instances such that the maximum number of voters need to be relocated
-    to these states in order to get at most ec_votes 
+    to these states in order to get at most ec_votes
     The empty list, if no possible states
     """
     pass
     #TODO
-    
+
 def move_min_voters(lost_states, ec_votes_needed):
     """
     Finds a subset of lost_states that would change an election outcome if
-    voters moved into those states. Should minimize the number of voters being relocated. 
+    voters moved into those states. Should minimize the number of voters being relocated.
     Only return states that were originally lost by the loser (won by the winner)
     of the election.
     Hint: This problem is simply the complement of dp_move_max_voters
@@ -216,24 +243,24 @@ def move_min_voters(lost_states, ec_votes_needed):
 #Problem 6
 def flip_election(election, swing_states):
     """
-    Finds a way to shuffle voters in order to flip an election outcome. 
-    Moves voters from states that were won by the losing candidate (any state not in lost_states), 
-    to each of the states in swing_states. To win a swing state, must move (margin + 1) new voters into that state. 
-    Also finds the number of EC votes gained by this rearrangement, as well as the minimum number of 
+    Finds a way to shuffle voters in order to flip an election outcome.
+    Moves voters from states that were won by the losing candidate (any state not in lost_states),
+    to each of the states in swing_states. To win a swing state, must move (margin + 1) new voters into that state.
+    Also finds the number of EC votes gained by this rearrangement, as well as the minimum number of
     voters that need to be moved.
 
     Parameters:
-    election - a list of State instances representing the election 
-    swing_states - a list of State instances where people need to move to flip the election outcome 
+    election - a list of State instances representing the election
+    swing_states - a list of State instances where people need to move to flip the election outcome
                    (result of move_min_voters or greedy_election)
-    
+
     Return:
     A tuple that has 3 elements in the following order:
-        - a dictionary with the following (key, value) mapping: 
-            - Key: a 2 element tuple, (from_state, to_state), the 2 letter abbreviation of the State 
-            - Value: int, number of people that are being moved 
-        - an int, the total number of EC votes gained by moving the voters 
-        - an int, the total number of voters moved 
+        - a dictionary with the following (key, value) mapping:
+            - Key: a 2 element tuple, (from_state, to_state), the 2 letter abbreviation of the State
+            - Value: int, number of people that are being moved
+        - an int, the total number of EC votes gained by moving the voters
+        - an int, the total number of voters moved
     None, if it is not possible to sway the election
     """
     pass
@@ -243,17 +270,17 @@ if __name__ == "__main__":
     pass
     # Uncomment the following lines to test each of the problems
 
-    # # tests Problem 1 and Problem 2 
-    # election2012 = load_election_results("2012_results.txt")
+    # # tests Problem 1 and Problem 2
+    election2012 = load_election_results("2012_results.txt")
 
-    # # tests Problem 3  
-    # winner, loser = find_winner(election2012)
-    # lost_states = states_lost(election2012)
-    # names_lost_states = [state.get_name() for state in lost_states]
-    # ec_votes_needed = ec_votes_reqd(election2012)
-    # print("Winner:", winner, "\nLoser:", loser)
-    # print("EC votes needed:",ec_votes_needed)
-    # print("States lost by the loser: ", names_lost_states, "\n")
+    # # tests Problem 3
+    winner, loser = find_winner(election2012)
+    lost_states = states_lost(election2012)
+    names_lost_states = [state.get_name() for state in lost_states]
+    ec_votes_needed = ec_votes_reqd(election2012)
+    print("Winner:", winner, "\nLoser:", loser)
+    print("EC votes needed:",ec_votes_needed)
+    print("States lost by the loser: ", names_lost_states, "\n")
 
     # tests Problem 4
     # print("greedy_election")
